168581bf871851f15891f75ad4245055ae03bb84
Icham Jaber <ijaber@student.42.fr>
2024-12-01 07:19:26 +0100

    add player pos at start

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 13:25:01 by ijaber            #+#    #+#             */
-/*   Updated: 2024/12/01 07:15:07 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/12/01 07:18:00 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int ac, char **av)
 	init_cub(&data, av);
 	check_file_cub(av[1]);
 	start_parse(&data, av);
-	// test_map_parsing(&data), affiche_texture_debug(data);
+	test_map_parsing(&data), affiche_texture_debug(data);
 	start_mlx(&data);
 	gc_free(DESTROY_PTR);
 	exit(EXIT_SUCCESS);
